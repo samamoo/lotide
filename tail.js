3f88b98affa02b89aaf6074c2f315894795b712f
Samantha Fok <samantha.fok@hotmail.com>
2020-11-02 21:21:58 +0000

    Refactor'd to include module.exports so that the function can be called anywhere

--- tail.js
+++ tail.js
@@ -1,17 +1,23 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.assert(false, `🔴 ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.assert(false, `🔴 ${actual} !== ${expected}`);
+//   }
+// };
+
+// const assertEqual = require("./assertEqual");
 
 const tail = function(array) {
   let chopped = [];
   chopped = array.slice(1);
   return chopped;
 };
-
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-assertEqual(words.length, 3);
+const animals = ["frog", "skunk", "badger", "quokka"]
+// console.log(tail(words));
+// console.log(tail(animals))
+
+// assertEqual(tail(words).length, 2);
+// assertEqual(tail(animals).length, 3)
+module.exports = tail;
