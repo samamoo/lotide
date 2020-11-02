fa369a74638accd923c5f7d1d2acd90706dcf38e
Samantha Fok <samantha.fok@hotmail.com>
2020-11-02 22:53:15 +0000

    Added module.export and required eqArrays

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,15 +1,4 @@
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays.js');
 
 const assertArrayEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -25,7 +14,9 @@ const assertArrayEqual = function(array1, array2) {
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual([1, 2, 3], [3, 2, 1]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
+// assertArrayEqual([1, 2, 3], [1, 2, 3]);
+// assertArrayEqual([1, 2, 3], [3, 2, 1]);
+// assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
+// assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
+
+module.exports = assertArrayEqual;
