e502adb6bc65f9edf501640cec14b4be8dbb1e98
Samantha Fok <samantha.fok@hotmail.com>
2020-11-03 00:33:43 +0000

    Modified test file to use Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,12 @@
+const assert = require('chai').assert;
 const tail = require("../tail.js");
-const assertEqual = require("../assertEqual.js");
 
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
-assertEqual(tail(["frog", "skunk", "badger", "quokka"]).length, 3)
+
+describe("tail of an array", function() {
+  it("should return length of the remaining array to be 2", function() {
+    assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
+  });
+  it("should return length of the remaining array to be 3", function() {
+    assert.strictEqual(tail(["frog", "skunk", "badger", "quokka"]).length, 3);
+  });
+})
