fbf4b89b3a4c16c8da334b2d3268d8096317095c
Samantha Fok <samantha.fok@hotmail.com>
2020-11-03 02:24:51 +0000

    Modified test file to include Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,17 @@
-const eqArrays = require('../eqArrays.js');
-const assertArrayEqual = require('../assertArrayEqual.js');
+const assert = require('chai').assert;
 const middle = require('../middle.js')
 
-assertArrayEqual(middle([1, 2, 3]), [2])
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+// assertArrayEqual(middle([1, 2, 3]), [2])
+// assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+
+describe("#middle", function() {
+  // it("should return middle array [2]", function() {
+  //   assert(middle([1, 2, 3]), [2]);    //THIS CAN WORK TOO.
+  // });
+  it("should return true when comparing middle values", function() {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  });
+  it("should return [3,4] when given array", function() {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+})
