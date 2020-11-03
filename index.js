c439e3cf2eb7f54f7eb8b7ff31ede528672b84ed
Samantha Fok <samantha.fok@hotmail.com>
2020-11-03 04:24:37 +0000

    Exporting all lotide files into one index

---
+++ index.js
@@ -0,0 +1,37 @@
+const assertArrayEqual = require('./assertArrayEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPosition = require('./letterPosition');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+
+module.exports = {
+  assertArrayEqual: assertArrayEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPosition: letterPosition,
+  map: map,
+  takeUntil: takeUntil,
+
+
+};
