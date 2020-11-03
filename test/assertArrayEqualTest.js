7eaeb9438a630e3c550f037a64a7cda066d93f2c
Samantha Fok <samantha.fok@hotmail.com>
2020-11-03 00:25:01 +0000

    Modified test file with Mocha and Chai

--- test/assertArrayEqualTest.js
+++ test/assertArrayEqualTest.js
@@ -1,7 +1,18 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays.js');
-const assertArrayEqual = require('../assertArrayEqual.js');
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual([1, 2, 3], [3, 2, 1]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
+
+describe("Asserting Arrays", function() {
+  it("should return true if array [1,2,3] equals [1,2,3]", function() {
+    assert.strictEqual(eqArrays([1,2,3], [1,2,3]), true);
+  });
+  it("should return false if array [1,2,3] equals [3,2,1]", function() {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("should return true if ['1','2','3'] equals ['1','2','3']", function() {
+    assert.strictEqual(eqArrays(['1','2','3'],['1','2','3']), true);
+  });
+  it ("should return false if ['1','2','3'] equals ['1','2',3]", function() {
+    assert.strictEqual(eqArrays(['1','2','3'], ['1','2',3]), false)
+  })
+})
